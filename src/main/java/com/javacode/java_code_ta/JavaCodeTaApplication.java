37e64248245ff699d7e1b85faeff93d6c4bd1a3a
MaximGuseynov <MaxGoodfella@yandex.ru>
2024-09-21 18:15:40 +0500

    Initial commit

---
+++ src/main/java/com/javacode/java_code_ta/JavaCodeTaApplication.java
@@ -0,0 +1,13 @@
+package com.javacode.java_code_ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeTaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeTaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javacode/java_code_ta/JavaCodeTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacode.java_code_ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeTaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
